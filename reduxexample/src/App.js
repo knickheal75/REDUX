9742b21c69e2a51b969deef1d9654c3d579bb934
knickheal75 <blitzkrieg0705@gmail.com>
2019-04-09 03:02:58 +0530

    form submit console

--- reduxexample/src/App.js
+++ reduxexample/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
+
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-       
+       <PostForm />
+       <hr />
         <Posts />
       </div>
     );
