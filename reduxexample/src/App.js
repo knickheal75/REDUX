c0c4c15adbdc9d59dc60d255b6403af4cbf2fb7d
knickheal75 <blitzkrieg0705@gmail.com>
2019-04-10 00:09:26 +0530

    adding live post full redux devtools addition

--- reduxexample/src/App.js
+++ reduxexample/src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
-
+import { Provider } from "react-redux";
+import store from "./store"
 
 class App extends Component {
   render(){
     return (
+      <Provider store={store}>
       <div className="App">
        <PostForm />
        <hr />
         <Posts />
       </div>
+      </Provider>
     );
   }
 }
